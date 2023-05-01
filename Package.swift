05a0004f70f11d3543f99f408e3b7072e517e13f
ryanaveo <ryanaveo@users.noreply.github.com>
2023-05-01 13:18:21 +0000

    Update SwiftSyntax

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMinor(from: "1.2.0")),
-        .package(url: "https://github.com/apple/swift-syntax.git", revision: "e8da93046936e02ef3d700527ecdcd808c3c6bcd"),
+        .package(url: "https://github.com/apple/swift-syntax.git", revision: "3c73cc88463aec967f4c6a2ac3e8de1f599e966a"),
         .package(url: "https://github.com/jpsim/SourceKitten.git", .upToNextMinor(from: "0.33.1")),
         .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.1"),
         .package(url: "https://github.com/scottrhoyt/SwiftyTextTable.git", from: "0.9.0"),
